55cecb1c23e39e72d0ed80651729fcf4c61bfc38
Daniel Abraham Braude <daniboy1007@gmail.com>
2020-10-14 18:57:05 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -7,5 +7,8 @@ def batch_badge_creator(names)
   names.map { |name| badge_maker(name) }
 end
 
-def assign_rooms
+def assign_rooms(speakers)
+  new_arr = []
+  speakers.each_with_index do |name, i|
+     new_arr << "Hello #{name}! You'll be assigned to room #{i+1}!"
 end
